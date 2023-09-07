31ab55ad2357e9e61550ca4d0d2e5cc6c3764741
Kalil <Kalil@anas-mbp.myfiosgateway.com>
2023-09-07 13:23:47 -0400

    modified .gitignore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Kalil";
+        String lastName = "Johnson";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
     }
 }
